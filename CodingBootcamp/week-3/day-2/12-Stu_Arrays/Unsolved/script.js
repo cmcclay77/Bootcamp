b9f9a44bb037ee54505420b01fe2fca737709e0f
Chris McClay <cmcclay77@gmail.com>
2022-10-21 19:09:06 -0400

    Initial

--- CodingBootcamp/week-3/day-2/12-Stu_Arrays/Unsolved/script.js
+++ CodingBootcamp/week-3/day-2/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,10 @@
 // WRITE YOUR CODE HERE
+var students = ["Chris", "Sean", "Joey", "Courtney"];
+console.log(students.length)
+
+students[0] = "Bob";
+
+console.log("Welcome to the class " + students[1]);
+if (students[0] === "Bob") {
+    console.log(students[0] + " is in class!");
+}
--- CodingBootcamp/week-3/day-2/20-Stu_Methods/Unsolved/script.js
+++ CodingBootcamp/week-3/day-2/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,12 @@ var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
 // WRITE YOUR CODE BELOW
+constellations.unshift("Canis Major");
+planets.pop();
+var galaxies = constellations.concat(planets);
+
+var space = star.toUpperCase();
+
+console.log(space);
+console.log(galaxies);
+console.log(constellations);

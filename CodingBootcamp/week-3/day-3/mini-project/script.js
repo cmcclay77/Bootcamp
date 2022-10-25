17916510140f10c95b18cdf6054dd30e551d30b9
Chris McClay <cmcclay77@gmail.com>
2022-10-24 22:16:34 -0400

    updated class work

---
+++ CodingBootcamp/week-3/day-3/mini-project/script.js
@@ -0,0 +1,26 @@
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame) {
+    let playerChoice = prompt("Please enter rock, paper or scissors.");
+    if (playerChoice) {
+        let playerOne = playerChoice.trim().toLowerCase();
+        if (
+            playerOne === "rock" ||
+            playerOne === "paper" ||
+            playerOne === "scissors"
+       )    {
+        let computerChoice = Math.floor(Math.random() * 3 + 1);
+        let computer =
+            computerChoice === 1
+                ? "rock"
+                : computerChoice === 2
+                ? "paper"
+                : "scissors";
+
+        let result =
+            playerOne === computer
+                ? "Tie game!"
+                : playerOne === "rock" && computer
+
+       }
+    }
+}

636da3f32764fb12ce3825d341500447f19c9369
Chris McClay <cmcclay77@gmail.com>
2022-10-24 20:36:32 -0400

    updated class work

--- CodingBootcamp/week-3/day-3/26-Stu_Object-This/Unsolved/script.js
+++ CodingBootcamp/week-3/day-3/26-Stu_Object-This/Unsolved/script.js
@@ -1,12 +1,12 @@
 // TODO: What does 'this' refer to?
-console.log(this);
+console.log(this); //This refers to the window
 
 // TODO: What does 'this' refer to?
 function helloThis() { 
-   console.log("Inside this function, this is " + this); 
+   console.log("Inside this function, this is " + this); // This refers to the object window
    }
 
-// TODO: What will this log? 
+// TODO: What will this log? // 20 years
 var child = { 
    age: 10,
    ageTenYears: function() {
@@ -14,7 +14,7 @@ var child = {
    }
 };
 
-// TODO: What will this log? 
+// TODO: What will this log? // 5000*1.5=5750
 var investor = {  
   name: "Cash Saver",
   investment: {
@@ -25,9 +25,9 @@ var investor = {
    }   
 };
 
-// TODO: After commenting, check your results!
+// TODO: After commenting, check your results! // listed in console
 helloThis();
 
-// TODO: After commenting, check your results!
+// TODO: After commenting, check your results! // Litsed in the console
 child.ageTenYears();
 investor.investment.investmentGrowth();

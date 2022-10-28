a1ee7eca1a99b510cb80149b2c2fccff71a01bbb
Chris McClay <cmcclay77@gmail.com>
2022-10-27 23:53:59 -0400

    updated classwork

--- CodingBootcamp/week-4/day-2/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ CodingBootcamp/week-4/day-2/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -3,5 +3,6 @@ var container = document.querySelector(".container");
 container.addEventListener("click", function(event) {
   var element = event.target;
 
-  // TODO: Complete function
-});
+ 
+
+  
